c0013ff606f8a3bfb623f967db514a3d0e4562d5
blankshiro <edwin.tok.2019@sis.smu.edu.sg>
2020-09-03 22:41:34 +0800

    Added Email Service (beta)

--- ryverbank/src/main/java/com/cs203t5/ryverbank/RyverbankApplication.java
+++ ryverbank/src/main/java/com/cs203t5/ryverbank/RyverbankApplication.java
@@ -1,13 +1,22 @@
 package com.cs203t5.ryverbank;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class RyverbankApplication {
 
+	@Autowired
+	private SendEmailService sendEmailService;
 	public static void main(String[] args) {
 		SpringApplication.run(RyverbankApplication.class, args);
 	}
+
+	@EventListener
+	public void triggerWhenStarts() {
+		sendEmailService.sendEmail("edwin.tok.2019@smu.edu.sg", "test", "test");
+	} 
 
 }
