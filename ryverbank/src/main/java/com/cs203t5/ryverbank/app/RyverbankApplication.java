5e3116660574fe3396eeaddc3b4510c24cbbdf21
Jun An <junan.tan.2019@sis.smu.edu.sg>
2020-09-22 10:31:09 +0800

    Fixing issues of parameter not found on master
    Scanning issue of package on spring, needed to declare the path of the component and entity in main class for spring application to work.

--- ryverbank/src/main/java/com/cs203t5/ryverbank/app/RyverbankApplication.java
+++ ryverbank/src/main/java/com/cs203t5/ryverbank/app/RyverbankApplication.java
@@ -4,10 +4,16 @@ package com.cs203t5.ryverbank.app;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 // import org.springframework.boot.context.event.ApplicationReadyEvent;
 // import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.cs203t5.ryverbank", "com.cs203t5"})
+@EnableJpaRepositories({"com.cs203t5.entity.Transaction", "com.cs203t5.entity.User"})
+@ComponentScan({"com.cs203t5.entity.Transaction", "com.cs203t5.entity.User"})
+@EntityScan({"com.cs203t5.entity.Transaction", "com.cs203t5.entity.User"})
 public class RyverbankApplication {
 
 	// @Autowired
--- ryverbank/src/main/java/com/cs203t5/ryverbank/entity/Transaction/TransactionRepository.java
+++ ryverbank/src/main/java/com/cs203t5/ryverbank/entity/Transaction/TransactionRepository.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionRepository extends JpaRepository <Transaction, Long>{
     List<Transaction> findByUserId(String userId);
     Optional<Transaction> findByIdAndUserId(Long id, String userId);
