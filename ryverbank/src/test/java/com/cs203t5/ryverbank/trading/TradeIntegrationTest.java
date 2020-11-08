566e67cff9316e20c960c16fd109be623a92e8bc
shiyinglee <61753966+shiyinglee@users.noreply.github.com>
2020-11-08 17:04:49 +0800

    update trade test case

---
+++ ryverbank/src/test/java/com/cs203t5/ryverbank/trading/TradeIntegrationTest.java
@@ -0,0 +1,417 @@
+package com.cs203t5.ryverbank.trading;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import net.minidev.json.JSONObject;
+
+import com.cs203t5.ryverbank.account_transaction.Account;
+import com.cs203t5.ryverbank.account_transaction.AccountRepository;
+import com.cs203t5.ryverbank.account_transaction.AccountServices;
+import com.cs203t5.ryverbank.customer.Customer;
+import com.cs203t5.ryverbank.customer.CustomerRepository;
+import com.cs203t5.ryverbank.customer.CustomerService;
+import com.cs203t5.ryverbank.portfolio.AssetService;
+import com.cs203t5.ryverbank.portfolio.Portfolio;
+import com.cs203t5.ryverbank.portfolio.PortfolioRepository;
+import com.cs203t5.ryverbank.portfolio.PortfolioService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.net.URI;
+import java.time.Instant;
+import java.util.Optional;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class TradeIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    private final String baseUrl = "http://localhost:";
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private TradeRepository tradeRepository;
+    
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PortfolioRepository portfolioRepository;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private AccountServices accountServices;
+
+    @Autowired
+    private PortfolioService portfolioServices;
+
+    @Autowired
+    private AssetService assetServices;
+
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+
+
+    @AfterEach
+    void tearDown(){
+        tradeRepository.deleteAll();
+        customerRepository.deleteAll();
+        portfolioRepository.deleteAll();
+        
+    }
+
+
+     
+
+    @Test
+    public void createTrade() throws Exception{
+
+        URI uri = new URI(baseUrl + port + "/trades");
+        long currentTimestamp = Instant.now().getEpochSecond();
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("user1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+   
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+
+
+        // Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("action", "buy");
+        jsonObject.put("symbol", "A17U");
+        jsonObject.put("quantity", 100);
+        jsonObject.put("bid", 0.0);
+        jsonObject.put("account_id", acc.getAccountID());
+
+
+        ResponseEntity<Trade> response = restTemplate.withBasicAuth("user1", "user1")
+        .postForEntity(uri, jsonObject, Trade.class);
+
+        // ResponseEntity<Trade> result = restTemplate.withBasicAuth("user1", "user1")
+        // .postForEntity(uri, trade, Trade.class);
+
+
+        assertEquals(201, response.getStatusCode().value());
+       
+    }
+ 
+
+    @Test
+    public void createTrade_InvalidCustomer() throws Exception{
+
+        URI uri = new URI(baseUrl + port + "/trades");
+        long currentTimestamp = Instant.now().getEpochSecond();
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("user1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+   
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+        Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+
+        ResponseEntity<Trade> result = restTemplate.withBasicAuth("user2", "user1")
+        .postForEntity(uri, trade, Trade.class);
+
+
+        assertEquals(401, result.getStatusCode().value());
+       
+    }
+
+    @Test
+    public void createTrade_AccountNotFound() throws Exception{
+
+        URI uri = new URI(baseUrl + port + "/trades");
+        long currentTimestamp = Instant.now().getEpochSecond();
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("user1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+        Customer customer2 = customerRepository.save(new Customer("user2", encoder.encode("goodpassword2"), "customer 2", "S9839006E",
+        "91251235", "Dog House", "ROLE_USER", true));
+   
+
+        Account acc2 = accountRepository.save(new Account(customer2.getCustomerId(), 80000.0, 80000.0));
+
+        Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc2.getAccountID(), customer.getCustomerId(), "open", 0.0));
+
+        ResponseEntity<Trade> result = restTemplate.withBasicAuth("user1", "user1")
+        .postForEntity(uri, trade, Trade.class);
+
+        assertEquals(404, result.getStatusCode().value());
+       
+    }
+
+
+  
+
+
+    @Test
+    public void getTrade_Success() throws Exception {
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+        long currentTimestamp = Instant.now().getEpochSecond();
+        Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+
+        URI uri = new URI(baseUrl + port + "/trades/" + trade.getId());
+
+        ResponseEntity<Object> response = restTemplate.withBasicAuth("user1", "goodpassword1")
+        .getForEntity(uri, Object.class);
+
+        assertEquals(200, response.getStatusCode().value());
+    }
+
+    @Test
+    public void getTrade_NotFound() throws Exception {
+        customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+
+        URI uri = new URI(baseUrl + port + "/trades/2");
+
+        ResponseEntity<Object> response = restTemplate.withBasicAuth("user1", "goodpassword1")
+        .getForEntity(uri, Object.class);
+
+        assertEquals(404, response.getStatusCode().value());
+    }
+
+    @Test
+    public void getTrade_UnauthorizeCustomer() throws Exception {
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+
+        Customer customer2 = customerRepository.save(new Customer("user2", encoder.encode("goodpassword2"), "customer 2", "S9839006E",
+        "91251235", "Dog House", "ROLE_USER", true));
+
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+        long currentTimestamp = Instant.now().getEpochSecond();
+        Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+
+        URI uri = new URI(baseUrl + port + "/trades/" + trade.getId());
+
+        ResponseEntity<Object> response = restTemplate.withBasicAuth("user2", "goodpassword2")
+        .getForEntity(uri, Object.class);
+
+        assertEquals(403, response.getStatusCode().value());
+    }
+
+    @Test
+    public void getTrade_invalidCustomer() throws Exception {
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+        long currentTimestamp = Instant.now().getEpochSecond();
+        Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+
+        URI uri = new URI(baseUrl + port + "/trades/" + trade.getId());
+
+        ResponseEntity<Object> response = restTemplate.withBasicAuth("user2", "goodpassword2")
+        .getForEntity(uri, Object.class);
+
+        assertEquals(401, response.getStatusCode().value());
+    }
+
+    @Test
+    public void cancelledTrade_Successful() throws Exception {
+        //For updates: We MUST create the HTTP headers with the new trade as the body 
+
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+
+
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+        long currentTimestamp = Instant.now().getEpochSecond();
+
+        Trade trade1 = tradeRepository.save(new Trade ("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+
+
+        // - IMPORTANT: Import the correct Http package - import org.springframework.http.HttpEntity;
+        HttpHeaders headers = new HttpHeaders();
+        
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+
+        Trade trade2 = tradeRepository.save(new Trade ("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "cancelled", 0.0));
+        HttpEntity<Trade> requestEntity = new HttpEntity<>(trade2,headers);
+
+        URI uri = new URI(baseUrl + port + "/trades/" + trade1.getId());
+
+        ResponseEntity<Trade> result = restTemplate.withBasicAuth("user1", "goodpassword1")
+        .exchange(uri, HttpMethod.PUT, requestEntity , Trade.class);
+
+        
+        assertEquals(200, result.getStatusCode().value());
+        assertNotNull(result);
+        assertEquals(trade2.getStatus(), result.getBody().getStatus());
+        assertEquals(trade1.getId(), result.getBody().getId());
+
+    }
+
+    @Test
+    public void cancelledTrade_unSuccessful() throws Exception {
+        //For updates: We MUST create the HTTP headers with the new trade as the body 
+
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+
+        Customer customer2 = customerRepository.save(new Customer("user2", encoder.encode("goodpassword2"), "customer 2", "S9839006E",
+        "91251235", "Dog House", "ROLE_USER", true));
+
+
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+        long currentTimestamp = Instant.now().getEpochSecond();
+
+        Trade trade1 = tradeRepository.save(new Trade ("buy", "A17U", 1000, 0.0, 0.0, 3.0, 1000, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "partial-filled", 3.0));
+
+
+        // - IMPORTANT: Import the correct Http package - import org.springframework.http.HttpEntity;
+        HttpHeaders headers = new HttpHeaders();
+        
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+
+        Trade trade2 = tradeRepository.save(new Trade ("buy", "A17U", 1000, 0.0, 0.0, 3.0, 1000, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "cancelled", 3.0));
+        HttpEntity<Trade> requestEntity = new HttpEntity<>(trade2,headers);
+
+        URI uri = new URI(baseUrl + port + "/trades/" + trade1.getId());
+  
+
+        ResponseEntity<Trade> result = restTemplate.withBasicAuth("user1", "goodpassword1")
+        .exchange(uri, HttpMethod.PUT, requestEntity , Trade.class);
+        
+
+        assertEquals(400, result.getStatusCode().value());
+
+    }
+
+
+    @Test
+    public void cancelledTrade_UnauthorizeCustomer() throws Exception {
+        //For updates: We MUST create the HTTP headers with the new trade as the body 
+
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+
+        Customer customer2 = customerRepository.save(new Customer("user2", encoder.encode("goodpassword2"), "customer 2", "S9839006E",
+        "91251235", "Dog House", "ROLE_USER", true));
+
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+        long currentTimestamp = Instant.now().getEpochSecond();
+
+        Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+    
+        // - IMPORTANT: Import the correct Http package - import org.springframework.http.HttpEntity;
+        HttpHeaders headers = new HttpHeaders();
+        
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+
+        Trade trade2 = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "cancelled", 0.0));
+        HttpEntity<Trade> requestEntity = new HttpEntity<>(trade2,headers);
+
+
+        URI uri = new URI(baseUrl + port + "/trades/" + trade.getId());
+
+        ResponseEntity<Trade> result = restTemplate.withBasicAuth("user2", "goodpassword2")
+        .exchange(uri, HttpMethod.PUT, requestEntity , Trade.class);
+        
+        assertEquals(403, result.getStatusCode().value());
+
+    }
+
+    @Test
+    public void cancelledTrade_NotFound() throws Exception {
+            //For updates: We MUST create the HTTP headers with the new trade as the body 
+
+            Customer customer = customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+            "91251234", "Dog House", "ROLE_USER", true));
+    
+            Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+    
+            long currentTimestamp = Instant.now().getEpochSecond();
+    
+            Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+        
+            // - IMPORTANT: Import the correct Http package - import org.springframework.http.HttpEntity;
+            HttpHeaders headers = new HttpHeaders();
+            
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            
+    
+            Trade trade2 = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "cancelled", 0.0));
+            HttpEntity<Trade> requestEntity = new HttpEntity<>(trade2,headers);
+    
+    
+            URI uri = new URI(baseUrl + port + "/trades/10");
+    
+            ResponseEntity<Trade> result = restTemplate.withBasicAuth("user1", "goodpassword1")
+            .exchange(uri, HttpMethod.PUT, requestEntity , Trade.class);
+            
+            assertEquals(404, result.getStatusCode().value());
+
+    }
+
+    @Test
+    public void cancelledTrade_InvalidCustomer() throws Exception {
+        //For updates: We MUST create the HTTP headers with the new trade as the body 
+
+        Customer customer = customerRepository.save(new Customer("user1", encoder.encode("goodpassword1"), "Woofy Dog", "S8529649C",
+        "91251234", "Dog House", "ROLE_USER", true));
+
+        Account acc = accountRepository.save(new Account(customer.getCustomerId(), 80000.0, 80000.0));
+
+        long currentTimestamp = Instant.now().getEpochSecond();
+
+        Trade trade = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "open", 0.0));
+    
+        // - IMPORTANT: Import the correct Http package - import org.springframework.http.HttpEntity;
+        HttpHeaders headers = new HttpHeaders();
+        
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        
+
+        Trade trade2 = tradeRepository.save(new Trade("buy", "A17U", 2000, 0.0,0.0, 0.0, 0, currentTimestamp, acc.getAccountID(), customer.getCustomerId(), "cancelled", 0.0));
+        HttpEntity<Trade> requestEntity = new HttpEntity<>(trade2,headers);
+
+
+        URI uri = new URI(baseUrl + port + "/trades/" + trade.getId());
+
+        ResponseEntity<Trade> result = restTemplate.withBasicAuth("user2", "goodpassword2")
+        .exchange(uri, HttpMethod.PUT, requestEntity , Trade.class);
+        
+        assertEquals(401, result.getStatusCode().value());
+
+    }
+
+
+    
+}
--- ryverbank/src/test/java/com/cs203t5/ryverbank/trading/TradeServiceTest.java
+++ ryverbank/src/test/java/com/cs203t5/ryverbank/trading/TradeServiceTest.java
@@ -163,4 +163,38 @@ public class TradeServiceTest {
       assertNull(updatedTrade);
 
    }
+
+   @Test
+   void cancelTrade_InvalidCustomer() {
+
+      int account_Id = 1;
+      int customer_Id = 4;
+      Long accountId = Long.valueOf(account_Id);
+      Long customerId = Long.valueOf(customer_Id);
+
+      // Arrange
+      Trade trade = new Trade("buy", "A17U", 2000, 0.0, 0.0, 0.0, 0, Instant.now().getEpochSecond(), accountId,
+            customerId, null,0.0);
+      Customer customer = new Customer("user1", "goodpassword1", "Ronald Trump", "S8529649C", "91251234", "White House",
+            "ROLE_USER", true);
+      Customer newCustomer = new Customer("user2", "goodpassword2", "Mark Tan", "S7982834C", "91431234", "White House",
+            "ROLE_USER", true);
+      Account account = new Account(customer.getCustomerId(), 500000, 500000);
+      accountServiceImpl.addAccount(account);
+      CustomStock customStock = new CustomStock("A17U", 3.25, 20000, 3.30, 20000, 3.30);
+
+      // Mocking the save
+      when(tradeRepository.save(any(Trade.class))).thenReturn(trade);
+
+      // Act: actually using the save option
+      userService.createUser(customer);
+      Trade savedTrade = tradeServiceImpl.createMarketBuyTrade(trade, customer, customStock);
+
+      // //Return a null
+      Trade updatedTrade = tradeServiceImpl.cancelTrade(savedTrade.getId(), newCustomer);
+      assertNull(updatedTrade);
+
+   }
+
+  
 }
